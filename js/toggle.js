3fc5549393a148f78ad4906300b942f44b7bb668
Kai Franke <cosinushh@gmail.com>
2022-09-12 16:37:40 +0200

    fix everything

--- js/toggle.js
+++ js/toggle.js
@@ -1,14 +1,15 @@
-function toggleButton() {
-  const cards = document.querySelectorAll("[data-js=card]");
-  const buttons = card.querySelectorAll("[data-js=buttons]");
+export default function toggleButton() {
+  const cards = document.querySelectorAll('[data-js=card]');
+  console.log(cards);
+  // const buttons = cards.querySelectorAll('[data-js=button]');
+  // console.log(buttons);
   cards.forEach(card => {
-        const button = card.querySelector('[data-js="button"]')
-        const link =  cards.querySelector('[data-js="link"]')
-        const arrow = card.querySelector('[data-js="arrow"]')
-          button.addEventListener('click',() => {
-          link.classList.toggle("hide")
-          arrow.classList.toggle("rotate")
-          
-    })
-})
+    const button = card.querySelector('[data-js="button"]');
+    const link = card.querySelector('[data-js="link"]');
+    const arrow = card.querySelector('[data-js="arrow"]');
+    button.addEventListener('click', () => {
+      link.classList.toggle('hidden');
+      arrow.classList.toggle('rotate');
+    });
+  });
 }
